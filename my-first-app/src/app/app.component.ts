a89cabb75835901b3e81e911bef39c0752ac0b73
mtunzisteven <mtunzism@byui.edu>
2022-04-24 01:33:07 +0200

    Week 1 done

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+ // <app-root></app-root>: This is the main element found in index.html, where we inject dynamic content into
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'my-first-app';
+  
 }
