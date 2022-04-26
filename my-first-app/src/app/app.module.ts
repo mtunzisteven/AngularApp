a5bee5475ab95760187e76404dc74d606c09e2ec
mtunzisteven <mtunzism@byui.edu>
2022-04-26 05:50:27 +0200

    learned about main.ts and app.component files relationship

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,3 +1,7 @@
+/**
+ * This file's content is exported into the 'src/main.ts' file and 
+ * is used as an arg to the bootstrap function
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -10,6 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // bootstrap array lists all the components angular should know it analyses the index.html. 
+  // It holds the keyword pointing to the following files for bootstrap to analyse:
+  // app.component.(css/html/spec.ts/ts) 
 })
 export class AppModule { }
--- my-first-app/src/main.ts
+++ my-first-app/src/main.ts
@@ -1,12 +1,23 @@
-import { enableProdMode } from '@angular/core';
+/**
+ * This is the file which holds the scripts that are inserrted into the finale html file served to browser
+ * It is the first file that get executed when we serve our app
+ */
+import { enableProdMode } from '@angular/core'; // this checks if we're on production mode or not and turns off some warnings respectively
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'; // the file within our app
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+/**
+ * The following line bootstraps(starts) our app by adding the app module as an argument to the function bootstrap(start) 
+ * When angular starts running, this line tells it to look inside app.module.ts file and in there it finds an array that gives 
+ * it information about the components it should use(app.component[css/html/spec.ts/ts]). In the app.component.ts file, it learns 
+ * the name of the html element it will inject the template into('selector'), as well as the files in which html template and its css
+ * are found within that root level
+ */
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
