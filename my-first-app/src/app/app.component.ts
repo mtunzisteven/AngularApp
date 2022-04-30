1ab21e878514c657bbbf1a814f6556cc71f209ab
Lusyluh <mav19003@byui.edu>
2022-05-01 01:06:25 +0200

    replaced the selector by class in the Component decorator wwith selector by app-root element

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,28 +1,20 @@
 import { Component } from '@angular/core';
 
-// Component decorator used
- // <app-root></app-root>: This is the main element found in index.html, where we inject dynamic content into
-@Component({
-  selector: '.app-root',
-  templateUrl: './app.component.html', // This tells us the html file that will serve as a template and who content will be injected into the selector
-  // styleUrls: ['./app.component.css'] // This tells us the file holding the css for the html template
-  styles:[`
-          h3{
-            color: green;
-          }
-        `]
-
-})
-
 /**
- * In the Cmponent decorator -> 
- * You can select the app component by html attribute or class as follows:
+ * In the Component decorator -> 
+ * You can select the app component by app-root html element, html attribute or class as follows:
+ *  By app-root html element
+ *    element in template: <app-root></app-root> 
+ *    selector: 'app-root'
+ * 
  *  By attribute
  *    element in template: <div app-server></div> 
  *    selector: '[app-server]'
+ * 
  *  By class:
  *    element in template: <div class='app-server'></div> 
  *    selector: '.app-server'
+ * 
  * Other css selectors don't work
  * 
  * You also can define the html and css inline within the decorator insteaed of poiniting to external files as we've done here:
@@ -39,6 +31,17 @@ import { Component } from '@angular/core';
  * The back ticks allow you to write the html or css in multiple lines. This method for adding these inline is useful when your 
  * lines of code are short. Otherwise, better to point to a file for each.
  */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html', // This tells us the html file that will serve as a template and who content will be injected into the selector
+  // styleUrls: ['./app.component.css'] // This tells us the file holding the css for the html template
+  styles:[`
+          h3{
+            color: green;
+          }
+        `]
+
+})
 
 export class AppComponent {
   
