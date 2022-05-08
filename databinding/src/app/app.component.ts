d76d5766aa2534a4e0dd38590e94d22e061924cf
Lusyluh <mav19003@byui.edu>
2022-05-08 07:37:33 +0200

    lesson  on binding completed

--- databinding/src/app/app.component.ts
+++ databinding/src/app/app.component.ts
@@ -27,4 +27,15 @@ export class AppComponent {
     });
   }
 
+  // For testing out life cycle hooks -------------------------------------------- |
+  onChangeFirst(){ 
+    console.log('Changed - Hit!');
+    this.serverElements[0].name = "Changed!";
+  }
+
+  onDestroyFirst(){ 
+    this.serverElements.splice(0, 1);
+  }
+
+  // ----------------------------------------------------------------------------- |
 }
--- databinding/src/app/server-element/server-element.component.ts
+++ databinding/src/app/server-element/server-element.component.ts
@@ -1,11 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  Input, 
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+      OnInit, 
+      OnChanges, 
+      DoCheck, 
+      AfterContentChecked,  
+      AfterContentInit,
+      AfterViewInit,
+      AfterViewChecked,
+      OnDestroy {
 
   // the @Input() decorator allows the element named 'element' in this case to be accessible in the parent component
   // without it, the element is only available in the 'server-element' component. We use 'Input' instead of 'Output' 
@@ -18,9 +41,70 @@ export class ServerElementComponent implements OnInit {
     content: string
   };
 
-  constructor() { }
+  @Input() name:string;
 
+  // accessing element by ref using @ViewChild and ElementRef
+  // this reference is on the html template of this component
+  @ViewChild('heading', {static: false}) header: ElementRef;
+
+  // accessing element by ref using @ContentChild and ElementRef
+  // this reference is on the html template of the parent component
+  // static: true, because we'll use this element inside ngOnInit(fired after ngOnInit())
+  @ContentChild('paragraph', {static: true}) paragraph: ElementRef;
+
+  constructor() { 
+    console.log('Contructor called!');
+  }
+
+  // called whenever a new instasnce of this component is called(loaded)
+  // or when changes occur(reloaded/changed value).
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log(changes);
+
+  }
+
+  // called whenever Angular checks for changes
+  ngDoCheck(): void{
+    console.log('ngDoCheck called!');
+
+  }
+
+  // called whenever ng-content in the component is loaded or reloaded(after change detection cycle)
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  // called whenever ng-content in the component is loaded: only once
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+
+  }
+
+   // called whenever ng-content in the component is loaded or reloaded(after change detection cycle)
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  // called whenever ng-content in the component is loaded: only once
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+    console.log('Element TextContent After VI: '+ this.header.nativeElement.textContent); // using element content before view initialized
+    console.log('Paragraph TextContent After ngOnInit: '+ this.paragraph.nativeElement.textContent); // using element content before view initialized
+
+  }
+
+  // called whenever ng-content in the component is loaded: only once
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
+  }
+
+  // called whenever a new instasnce of this component is called(loaded)
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('Element TextContent Before VI: '+ this.header.nativeElement.textContent); // using element content before view initialized
+    console.log('Paragraph TextContent Before ngOnInit: '+ this.paragraph.nativeElement.textContent); // using element content before view initialized
+
   }
 
 }
