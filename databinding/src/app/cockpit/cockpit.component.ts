3f08fb2fb8d33cb282a641426b76022da17e87d0
Lusyluh <mav19003@byui.edu>
2022-05-08 05:16:43 +0200

    ng-content learned and understood

--- databinding/src/app/cockpit/cockpit.component.ts
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -15,7 +15,10 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  // Accessing an element ref passed directly from the DOM using the reference
+  @ViewChild('serverContentInput', {static: false}) serverContentInput: ElementRef;
   
   constructor() { }
 
@@ -23,19 +26,28 @@ export class CockpitComponent implements OnInit {
   }
 
   // the function that emits the serverCreated event and the input data
+  // it takes as an argument, an input element passed directly from the DOM using the reference
   onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverNameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value // nativeElement gives us acces to the actual element obtained through ElementRef
     });
   }
 
   // the function that emits the blueprintCreated event and the input data
+  // it takes as an argument, an input element passed directly from the DOM using the reference
   onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: serverNameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value // nativeElement gives us acces to the actual element obtained through ElementRef
     });
   }
+
+  /**
+   * NOTE: 
+   * • Never access and change dom using element.value obtained using any of the methods above
+   * • Better use string interpolation to do that instead of using references.
+   * 
+   */
 
 }
