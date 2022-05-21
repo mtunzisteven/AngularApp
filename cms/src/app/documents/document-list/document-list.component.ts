52223a34193857a35f52273d3713484ac5b4f370
mtunzisteven <mav19004@byui.edu>
2022-05-21 19:13:09 +0200

    Week 4 complete

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  //  id, name, description and url
+  documents: Document[] = [
+    new Document('001', 'WDD330', 'Front end web development 1', 'www.byui.edu', null),
+    new Document('002', 'WDD331', 'Front end web development', 'www.byui.edu', null),
+    new Document('003', 'WDD440', 'Web full-stack development', 'www.byui.edu', null),
+    new Document('004', 'CSE340', 'Back end web development 1', 'www.byui.edu', null),
+    new Document('005', 'CSE341', 'Back end web development 2', 'www.byui.edu', null)
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
